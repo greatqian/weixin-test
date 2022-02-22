b675fd8e14de170c6e845160a112770ff8214a7f
greatqian <qdaxia666@gmail.com>
2022-02-22 15:31:46 +0800

    init

---
+++ src/main/java/com/tencent/wxcloudrun/controller/TokenController.java
@@ -0,0 +1,32 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.utils.CheckoutUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author greatqian
+ */
+@RestController
+public class TokenController {
+
+    @GetMapping("/hello")
+    public String hello(HttpServletRequest request) {
+        // 微信加密签名
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (signature != null && CheckoutUtil.checkSignature(signature, timestamp, nonce)) {
+            return echostr;
+        }
+        return "无效参数";
+    }
+
+}
